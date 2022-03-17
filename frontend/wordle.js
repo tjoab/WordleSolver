a0f867c4c9f063232a877bab3eda58e75c083df0
TJ Ayoub <tj.ayoub@gmail.com>
2022-03-17 09:05:06 -0400

    repetitive code made tidy

--- frontend/wordle.js
+++ frontend/wordle.js
@@ -12,51 +12,18 @@ window.onload = function(){
 }
 
 function intialize() {
-    let t1 = document.getElementById('tile1');
-    t1.addEventListener('click', function (event) {
-        var currClass = t1.className
-        t1.classList.remove(currClass);
-        if (currClass === "empty")
-            currClass = "correct";
-        t1.classList.add(classList[(classIndex[currClass] + 1) % 3]);
-    });
 
-    let t2 = document.getElementById('tile2');
-    t2.addEventListener('click', function (event) {
-        var currClass = t2.className
-        t2.classList.remove(currClass);
-        if (currClass === "empty")
-            currClass = "correct";
-        t2.classList.add(classList[(classIndex[currClass] + 1) % 3]);
-    });
-
-    let t3 = document.getElementById('tile3');
-    t3.addEventListener('click', function (event) {
-        var currClass = t3.className
-        t3.classList.remove(currClass);
-        if (currClass === "empty")
-            currClass = "correct";
-        t3.classList.add(classList[(classIndex[currClass] + 1) % 3]);
-    });
-
-    let t4 = document.getElementById('tile4');
-    t4.addEventListener('click', function (event) {
-        var currClass = t4.className
-        t4.classList.remove(currClass);
-        if (currClass === "empty")
-            currClass = "correct";
-        t4.classList.add(classList[(classIndex[currClass] + 1) % 3]);
-    });
-
-    let t5 = document.getElementById('tile5');
-    t5.addEventListener('click', function (event) {
-        var currClass = t5.className
-        t5.classList.remove(currClass);
-        if (currClass === "empty")
-            currClass = "correct";
-        t5.classList.add(classList[(classIndex[currClass] + 1) % 3]);
-    });
-
+    for (let i = 1; i <= maxCol; i++){
+        let t = document.getElementById("tile".concat(i.toString()))
+        t.addEventListener('click', function (event) {
+            var currClass = t.className
+            t.classList.remove(currClass);
+            if (currClass === "empty")
+                currClass = "correct";
+            t.classList.add(classList[(classIndex[currClass] + 1) % 3]);
+        });
+    }
+                
     let keyboardRows = document.getElementsByClassName('keyboardRow');
     
     
@@ -80,6 +47,7 @@ function processKey() {
     e = { "code" : this.id };
     processInput(e);
 }
+
 
 function processInput(e) {
 
@@ -110,34 +78,13 @@ function processInput(e) {
         if (col-1 == 5 && patternEntered){
             let updateMes = document.getElementById("updateMessage").children[0];
             updateMes.textContent = "";
-
-            let t1 = document.getElementById('tile1');
-            var currClass = t1.className
-            t1.classList.remove(currClass);
-            t1.classList.add('empty')
-
-            let t2 = document.getElementById('tile2');
-            var currClass = t2.className
-            t2.classList.remove(currClass);
-            t2.classList.add('empty')
-
-            let t3 = document.getElementById('tile3');
-            var currClass = t3.className
-            t3.classList.remove(currClass);
-            t3.classList.add('empty')
-
-            let t4 = document.getElementById('tile4');
-            var currClass = t4.className
-            t4.classList.remove(currClass);
-            t4.classList.add('empty')
-
-            let t5 = document.getElementById('tile5');
-            var currClass = t5.className
-            t5.classList.remove(currClass);
-            t5.classList.add('empty')
-
-            for (let i = 1; i <= maxCol; i++)
-                document.getElementById("tile".concat(i.toString())).textContent = " ";
+            for (let i = 1; i <= maxCol; i++){
+                let t = document.getElementById("tile".concat(i.toString()))
+                var currClass = t.className
+                t.classList.remove(currClass);
+                t.classList.add('empty')
+                t.textContent = " ";
+            }
             col = 1;
             // Send to python app
             // Display results
