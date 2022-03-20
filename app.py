952fe3ae5fe1f234ea53678648d3458ebb467809
tjoab <65689829+tjoab@users.noreply.github.com>
2022-03-20 12:01:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,16 +26,19 @@ myWordList = getWordsFromFile("allowed_words.txt")
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global userGuess, userPattern
-
+    print("outside")
     if request.method == "POST":
         userGuess = request.form['userGuess']
         userPattern = request.form['userPattern']
+        print(userGuess)
+        print(userPattern)
         
     return render_template("wordle.html")
 
 
 @app.route('/restart', methods=['POST'])
 def restart():
+    print("restart")
     global myWordList
     if request.method == "POST":
         myWordList = getWordsFromFile("allowed_words.txt")
@@ -44,9 +47,11 @@ def restart():
 
 @app.route('/getpythondata')
 def get_python_data():
-    time.sleep(1)
+    time.sleep(1.5)
     global userGuess, userPattern, frequencyWeights, myWordList
-
+    print("get")
+    print(userGuess)
+    print(userPattern)
     if len(myWordList) != 1:
         myWordList = getResultantWords(userGuess.lower(), userPattern, myWordList)
         
